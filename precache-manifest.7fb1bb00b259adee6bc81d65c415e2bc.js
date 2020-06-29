b244d1a6bfb1af7bac3aeaddee74b6adc7b14e05
Sarthakux <ysarthak275@gmail.com>
2020-06-29 11:58:21 +0530

    Updates

---
+++ precache-manifest.7fb1bb00b259adee6bc81d65c415e2bc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "343313c3e934d4e2fb83ac789a908841",
+    "url": "/covid-tracker/index.html"
+  },
+  {
+    "revision": "dc81d4d1e7942c84976e",
+    "url": "/covid-tracker/static/css/main.17c22fdb.chunk.css"
+  },
+  {
+    "revision": "3b8fd1cbb23c59e7a913",
+    "url": "/covid-tracker/static/js/2.3efc8833.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/covid-tracker/static/js/2.3efc8833.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dc81d4d1e7942c84976e",
+    "url": "/covid-tracker/static/js/main.3e40146b.chunk.js"
+  },
+  {
+    "revision": "a2c09be945803fa375fb",
+    "url": "/covid-tracker/static/js/runtime-main.948b6ba1.js"
+  },
+  {
+    "revision": "0209d23888c2bfde2f2efdf9ff7dfebc",
+    "url": "/covid-tracker/static/media/image.0209d238.jpeg"
+  }
+]);
